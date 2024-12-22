715859b152f246e98c49563e6b266158a47b8c38
Faez-B <faezbhatti@gmail.com>
2024-12-22 22:47:42 +0100

    ✨ : display Task form   fix: missing migration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Task;
+use App\Form\TaskType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('home/index.html.twig');
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($task);
+            $em->flush();
+        }
+
+        return $this->render('home/index.html.twig', [
+            'form' => $form,
+        ]);
     }
 }

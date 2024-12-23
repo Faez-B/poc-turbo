fc6a1dfdf8c997505144477850053c8776f24bf5
Faez-B <faezbhatti@gmail.com>
2024-12-23 12:26:28 +0100

    💄: using pico css & js   ♻️: removed unnecessary stimulus controller

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,9 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
 import '@picocss/pico/css/pico.min.css';
+import '@picocss/pico';
+
+import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');

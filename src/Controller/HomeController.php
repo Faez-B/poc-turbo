a927f2712d421c42b72c83828c4ee0ea3ad64482
Faez-B <faezbhatti@gmail.com>
2024-12-23 13:11:40 +0100

    ✨: display tasks list ✨: render a new Task using turbo stream

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Form\TaskType;
+use Symfony\UX\Turbo\TurboBundle;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +16,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $tasks = $em->getRepository(Task::class)->findAll();
+
         $task = new Task();
         $form = $this->createForm(TaskType::class, $task);
 
@@ -23,11 +26,18 @@ class HomeController extends AbstractController
             $em->persist($task);
             $em->flush();
 
-            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+            // 🔥 The magic happens here! 🔥
+            if (TurboBundle::STREAM_FORMAT === $request->getPreferredFormat()) {
+                // If the request comes from Turbo, set the content type as text/vnd.turbo-stream.html and only send the HTML to update
+                $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+                return $this->renderBlock('home/index.html.twig', 'success_stream', ['task' => $task]);
+            }
         }
 
         return $this->render('home/index.html.twig', [
             'form' => $form,
+            'task' => $task,
+            'tasks' => $tasks,
         ]);
     }
 }

8b4f681f35386c723f0692483e0d295e4e4df3ad
Faez-B <faezbhatti@gmail.com>
2024-12-24 10:01:18 +0100

    ✨: using same logic as Task to update Messages list

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -2,15 +2,48 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Message;
+use App\Form\MessageType;
+use Symfony\UX\Turbo\TurboBundle;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ChatController extends AbstractController
 {
     #[Route('/chat', name: 'chat')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('chat/index.html.twig');
+        $messages = $em->getRepository(Message::class)->findAll();
+        $action = count($messages) === 0 ? 'message_replace' : 'message_append';
+
+        $message = new Message();
+        $form = $this->createForm(MessageType::class, $message);
+        $emptyForm = clone $form;
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($message);
+            $em->flush();
+
+            $block = $this->render('broadcast/' . $action . '.stream.html.twig', [
+                'message' => $message->getContent(),
+                'form' => $emptyForm,
+            ]);
+
+            if (TurboBundle::STREAM_FORMAT === $request->getPreferredFormat()) {
+                $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+                return $block;
+            }
+        }
+
+        return $this->render('chat/index.html.twig', [
+            'form' => $form,
+            'message' => $message,
+            'messages' => $messages,
+        ]);
     }
 }
